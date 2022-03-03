acfe99d5895f232b6c547cd7a7d6c6e92ac44b67
Adrian Istrate <adr_istrate@yahoo.com>
2022-03-03 18:13:30 +0200

    Show a point on the chart where the user is clicking.

--- src/coffeeShops/CoffeeShopsChart.tsx
+++ src/coffeeShops/CoffeeShopsChart.tsx
@@ -9,7 +9,7 @@ import {
   Tooltip,
   TooltipItem,
 } from "chart.js";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Scatter } from "react-chartjs-2";
 import { CoffeeShop } from "./models";
 
@@ -22,30 +22,43 @@ type CoffeeShopsChartProps = {
 export default function CoffeeShopsChart({
   coffeeShops,
 }: CoffeeShopsChartProps) {
-  const options = createOptions(onClick);
-  const data = createData(coffeeShops);
   const chartRef = useRef<Chart<"scatter">>(null!);
+  const [location, setLocation] = useState<Point | null>(null);
+
+  const data = {
+    datasets: [allShopsDataset(coffeeShops)],
+  };
+  if (location) {
+    data.datasets.push(locationDataset(location));
+  }
+
+  const options = createOptions(onClick);
 
   function onClick(event: ChartEvent) {
-    const { x, y } = toChartCoordinates(
+    const point = toChartCoordinates(
       event.x || 0,
       event.y || 0,
       chartRef.current.chartArea
     );
 
-    if (isPointWithinBounds(x, y)) {
-      const data = [
-        {
-          name: "Current location",
-          x,
-          y,
-        },
-      ];
+    if (isWithinBounds(point)) {
+      setLocation(point);
     }
   }
 
   return <Scatter options={options} data={data} ref={chartRef} />;
 }
+
+type Point = { x: number; y: number };
+
+type DataPoint = { title: string } & Point;
+
+type Dataset = {
+  label: string;
+  data: DataPoint[];
+  borderColor: string;
+  backgroundColor: string;
+};
 
 function createOptions(onClick: (event: ChartEvent) => void) {
   return {
@@ -78,7 +91,7 @@ function createOptions(onClick: (event: ChartEvent) => void) {
 }
 
 function tooltipTitle(tooltipItems: TooltipItem<"scatter">[]) {
-  return tooltipItems.map((item) => (item.raw as CoffeeShop).name).join("\n");
+  return tooltipItems.map((item) => (item.raw as DataPoint).title).join("\n");
 }
 
 function toChartCoordinates(
@@ -99,23 +112,34 @@ function toChartCoordinates(
   return { x, y };
 }
 
-function isPointWithinBounds(x: number, y: number) {
+function isWithinBounds({ x, y }: Point) {
   return -180 <= x && x <= 180 && -90 <= y && y <= 90;
 }
 
-function createData(coffeeShops: CoffeeShop[]) {
+function allShopsDataset(coffeeShops: CoffeeShop[]): Dataset {
   return {
-    datasets: [
+    label: "All coffee shops",
+    data: coffeeShops.map((coffeeShop) => ({
+      title: coffeeShop.name,
+      x: coffeeShop.y,
+      y: coffeeShop.x,
+    })),
+    borderColor: "rgba(53, 162, 235, 1)",
+    backgroundColor: "rgba(53, 162, 235, 0.5)",
+  };
+}
+
+function locationDataset({ x, y }: Point): Dataset {
+  return {
+    label: "Current location",
+    data: [
       {
-        label: "All Blue Bottle Coffee Shops",
-        data: coffeeShops.map((coffeeShop) => ({
-          name: coffeeShop.name,
-          x: coffeeShop.y,
-          y: coffeeShop.x,
-        })),
-        borderColor: "rgba(53, 162, 235, 1)",
-        backgroundColor: "rgba(53, 162, 235, 0.5)",
+        title: "Current location",
+        x,
+        y,
       },
     ],
+    borderColor: "rgba(53, 235, 162, 1)",
+    backgroundColor: "rgba(53, 235, 162, 0.5)",
   };
 }
